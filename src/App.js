8f8d52cda22867903cb2e1e4ccc07529f496b4b8
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-12-14 13:08:12 +0100

    setting up basic html layout and axios request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+    async function fetchData() {
+        try {
+            const result = await axios.get('https://pokeapi.co/api/v2/pokemon/ditto', {
+                    'Accept': 'application/json',
+                });
+            console.log(result)
+        } catch (e) {
+        console.error(e);
+        }
+    }
+
+
   return (
     <div>
-      Begin hier met de opdracht!
+        <img src="" alt="logo"/>
+        <div className="buttons">
+            <button
+            type="button"
+            onClick={fetchData}
+            >
+                Vorige
+            </button>
+            <button
+                type="button"
+            >
+                Volgende
+            </button>
+        </div>
+        <div className="pokemons">
+        <div className="pokemon">
+            <h3>name</h3>
+            <img src="" alt="pokemon"/>
+            <h4>Moves: </h4>
+            <h4>Weight: </h4>
+            <h4>Abilities:</h4>
+            <div className="abilities">
+            </div>
+        </div>
+        </div>
+
+
     </div>
   );
 }
